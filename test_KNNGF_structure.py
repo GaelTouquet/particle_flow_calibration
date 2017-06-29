6b09229c27ae63f4e2060e156ff45eef234f835e
Samuel Niang <samuel.niang@gmail.com>
2017-06-29 22:17:01 +0200

    limit on the distance of the neighbours

--- test_KNNGF_structure.py
+++ test_KNNGF_structure.py
@@ -10,7 +10,7 @@ Script to understand how does KNNGF works and why there is a structure in ecalib
 import matplotlib.pyplot as plt
 import numpy as np
 from pfcalibration.tools import importPickle, savefig
-from pfcalibration.tools import gaussian_fit, gaussian_param, binwidth_array
+from pfcalibration.tools import gaussian_fit, binwidth_array
 from sklearn import neighbors
 import math
 
@@ -92,35 +92,35 @@ plt.ylabel(r"$e_{calib}/e_{true}$",fontsize=15)
 plt.title(r"$e_{calib}/e_{true}$ for $e_{cal} = 0$",fontsize=15)
 savefig(fig,directory,"ecalib_over_etrue.png")
 
-#histogram of hcal
-fig = plt.figure(figsize=(10,5))
-bw = binwidth_array(h)
-entries, bins, other = plt.hist(h,bw)
-plt.xlabel(r"$h_{cal}$",fontsize=15)
-plt.title(r"$h_{cal}$ for $e_{cal} = 0$",fontsize=15)
-plt.show()
-savefig(fig,directory,"histograms_hcal.png")
-
-#histogram of ecalib
-fig = plt.figure(figsize=(10,5))
-#bw = binwidth_array(c)
-bw = binwidth_array(c,0.01)
-entries, bins, other = plt.hist(c,bw)
-plt.xlabel(r"$e_{calib}$",fontsize=15)
-plt.title(r"$e_{calib}$ for $e_{cal} = 0$",fontsize=15)
-plt.show()
-savefig(fig,directory,"histograms_ecalib.png")
-
-imax = np.argmax(entries)
-borne_inf = bins[imax]
-borne_sup = bins[imax+1]
-index = np.logical_and(c >= borne_inf,c < borne_sup)
-ecalibs = c[index]
-fig = plt.figure(figsize=(10,5))
-plt.hist(ecalibs,binwidth_array(ecalibs))
-plt.xlabel(r"$e_{calib}$",fontsize=15)
-plt.title(r"$e_{calib}$ for the highest bar",fontsize=15)
-savefig(fig,directory,"histograms_ecalib_etrue_highest.png")
+##histogram of hcal
+#fig = plt.figure(figsize=(10,5))
+#bw = binwidth_array(h)
+#entries, bins, other = plt.hist(h,bw)
+#plt.xlabel(r"$h_{cal}$",fontsize=15)
+#plt.title(r"$h_{cal}$ for $e_{cal} = 0$",fontsize=15)
+#plt.show()
+#savefig(fig,directory,"histograms_hcal.png")
+#
+##histogram of ecalib
+#fig = plt.figure(figsize=(10,5))
+##bw = binwidth_array(c)
+#bw = binwidth_array(c,0.01)
+#entries, bins, other = plt.hist(c,bw)
+#plt.xlabel(r"$e_{calib}$",fontsize=15)
+#plt.title(r"$e_{calib}$ for $e_{cal} = 0$",fontsize=15)
+#plt.show()
+#savefig(fig,directory,"histograms_ecalib.png")
+#
+#imax = np.argmax(entries)
+#borne_inf = bins[imax]
+#borne_sup = bins[imax+1]
+#index = np.logical_and(c >= borne_inf,c < borne_sup)
+#ecalibs = c[index]
+#fig = plt.figure(figsize=(10,5))
+#plt.hist(ecalibs,binwidth_array(ecalibs))
+#plt.xlabel(r"$e_{calib}$",fontsize=15)
+#plt.title(r"$e_{calib}$ for the highest bar",fontsize=15)
+#savefig(fig,directory,"histograms_ecalib_etrue_highest.png")
 
 #courbe de calibration pour ecal = 0
 hcal_train = KNNGF.hcal_train[KNNGF.ecal_train==0]
@@ -130,24 +130,24 @@ ecal_calib = np.zeros(len(hcal_calib))
 calib = KNNGF.predict(ecal_calib,hcal_calib)
 
 fig = plt.figure(figsize=(10,10))
-plt.subplot(2,1,1)
+#plt.subplot(2,1,1)
 plt.plot(hcal_train,true_train,'.',markersize=1,label=r"training data")
-plt.plot(h[index],t[index],'.',label=r"$e_{calib} \in ["+str(np.around(borne_inf,2))+","+str(np.around(borne_sup,2))+"[$")
+#plt.plot(h[index],t[index],'.',label=r"$e_{calib} \in ["+str(np.around(borne_inf,2))+","+str(np.around(borne_sup,2))+"[$")
 plt.plot(hcal_calib,calib,lw=3,label = "calibration")
 plt.xlabel(r"$h_{cal}$",fontsize=15)
 plt.ylabel(r"$e_{true}$",fontsize=15)
 plt.title(r"$e_{true}$ for $e_{cal} = 0$",fontsize=15)
 plt.axis([0,max(hcal_train),0,max(true_train)])
 plt.legend(loc = "upper right")
-
-plt.subplot(2,2,3)
-plt.plot(hcal_train,true_train,'.',markersize=1,label=r"training data")
-plt.plot(h[index],t[index],'.',label=r"$e_{calib} \in ["+str(np.around(borne_inf,2))+","+str(np.around(borne_sup,2))+"[$")
-plt.plot(hcal_calib,calib,lw=3,label = "calibration")
-plt.xlabel(r"$h_{cal}$",fontsize=15)
-plt.ylabel(r"$e_{true}$",fontsize=15)
-plt.title(r"$e_{true}$ for $e_{cal} = 0$",fontsize=15)
-plt.axis([min(h[index]),max(h[index]),0,2*borne_sup])
-
-
-savefig(fig,directory,"calibration.png")
+#
+#plt.subplot(2,2,3)
+#plt.plot(hcal_train,true_train,'.',markersize=1,label=r"training data")
+#plt.plot(h[index],t[index],'.',label=r"$e_{calib} \in ["+str(np.around(borne_inf,2))+","+str(np.around(borne_sup,2))+"[$")
+#plt.plot(hcal_calib,calib,lw=3,label = "calibration")
+#plt.xlabel(r"$h_{cal}$",fontsize=15)
+#plt.ylabel(r"$e_{true}$",fontsize=15)
+#plt.title(r"$e_{true}$ for $e_{cal} = 0$",fontsize=15)
+#plt.axis([min(h[index]),max(h[index]),0,2*borne_sup])
+#
+#
+#savefig(fig,directory,"calibration.png")
