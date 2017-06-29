4a78d815e29dd52028df32921102ca85c90ccc91
Samuel Niang <samuel.niang@gmail.com>
2017-06-29 21:42:09 +0200

    try to understand the structure in KNNGF 2

--- test_KNNGF_structure.py
+++ test_KNNGF_structure.py
@@ -92,20 +92,31 @@ plt.ylabel(r"$e_{calib}/e_{true}$",fontsize=15)
 plt.title(r"$e_{calib}/e_{true}$ for $e_{cal} = 0$",fontsize=15)
 savefig(fig,directory,"ecalib_over_etrue.png")
 
-#histogram of ecalib and etrue
+#histogram of hcal
 fig = plt.figure(figsize=(10,5))
-bw = binwidth_array(c)
+bw = binwidth_array(h)
+entries, bins, other = plt.hist(h,bw)
+plt.xlabel(r"$h_{cal}$",fontsize=15)
+plt.title(r"$h_{cal}$ for $e_{cal} = 0$",fontsize=15)
+plt.show()
+savefig(fig,directory,"histograms_hcal.png")
+
+#histogram of ecalib
+fig = plt.figure(figsize=(10,5))
+#bw = binwidth_array(c)
+bw = binwidth_array(c,0.01)
 entries, bins, other = plt.hist(c,bw)
 plt.xlabel(r"$e_{calib}$",fontsize=15)
 plt.title(r"$e_{calib}$ for $e_{cal} = 0$",fontsize=15)
 plt.show()
-savefig(fig,directory,"histograms_ecalib_etrue.png")
+savefig(fig,directory,"histograms_ecalib.png")
 
 imax = np.argmax(entries)
 borne_inf = bins[imax]
 borne_sup = bins[imax+1]
 index = np.logical_and(c >= borne_inf,c < borne_sup)
 ecalibs = c[index]
+fig = plt.figure(figsize=(10,5))
 plt.hist(ecalibs,binwidth_array(ecalibs))
 plt.xlabel(r"$e_{calib}$",fontsize=15)
 plt.title(r"$e_{calib}$ for the highest bar",fontsize=15)
@@ -117,15 +128,26 @@ true_train = KNNGF.true_train[KNNGF.ecal_train==0]
 hcal_calib = np.arange(min(hcal_train),lim,0.1)
 ecal_calib = np.zeros(len(hcal_calib))
 calib = KNNGF.predict(ecal_calib,hcal_calib)
-fig = plt.figure(figsize=(10,5))
 
+fig = plt.figure(figsize=(10,10))
+plt.subplot(2,1,1)
 plt.plot(hcal_train,true_train,'.',markersize=1,label=r"training data")
-plt.plot(hcal_calib,calib,lw=3,label = "calibration")
 plt.plot(h[index],t[index],'.',label=r"$e_{calib} \in ["+str(np.around(borne_inf,2))+","+str(np.around(borne_sup,2))+"[$")
-
+plt.plot(hcal_calib,calib,lw=3,label = "calibration")
 plt.xlabel(r"$h_{cal}$",fontsize=15)
 plt.ylabel(r"$e_{true}$",fontsize=15)
 plt.title(r"$e_{true}$ for $e_{cal} = 0$",fontsize=15)
 plt.axis([0,max(hcal_train),0,max(true_train)])
 plt.legend(loc = "upper right")
+
+plt.subplot(2,2,3)
+plt.plot(hcal_train,true_train,'.',markersize=1,label=r"training data")
+plt.plot(h[index],t[index],'.',label=r"$e_{calib} \in ["+str(np.around(borne_inf,2))+","+str(np.around(borne_sup,2))+"[$")
+plt.plot(hcal_calib,calib,lw=3,label = "calibration")
+plt.xlabel(r"$h_{cal}$",fontsize=15)
+plt.ylabel(r"$e_{true}$",fontsize=15)
+plt.title(r"$e_{true}$ for $e_{cal} = 0$",fontsize=15)
+plt.axis([min(h[index]),max(h[index]),0,2*borne_sup])
+
+
 savefig(fig,directory,"calibration.png")
