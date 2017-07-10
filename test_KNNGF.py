d25bae6e44a64680f4c1c5e9900fedcc5fc25359
Samuel Niang <samuel.niang@gmail.com>
2017-07-10 12:20:38 +0200

    README.md index.md

--- test_KNNGF.py
+++ test_KNNGF.py
@@ -34,7 +34,7 @@ data1,data2 = data1.splitInTwo()
 # parameters of the calibration
 lim = 150                   # if ecal + hcal > lim, ecalib = math.nan
 n_neighbors_ecal_eq_0=2000  # number of neighbors for ecal = 0
-n_neighbors_ecal_neq_0=250  # number of neighbors for ecal ≠ 0
+n_neighbors_ecal_neq_0=1000  # number of neighbors for ecal ≠ 0
 energystep = 1
 # We create the calibration
 KNNGF = data1.KNNGaussianFit(n_neighbors_ecal_eq_0=n_neighbors_ecal_eq_0,
