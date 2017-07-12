aca6c5bc7c822aaf9e5651f384902c9ae0e91796
Samuel Niang <samuel.niang@gmail.com>
2017-07-12 14:47:51 +0200

    example_KNNGF.py

---
+++ example_KNNGF.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Developed by Samuel Niang
+For IPNL (Nuclear Physics Institute of Lyon)
+
+Script to understand how does KNNGF works.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pfcalibration.usualplots as usplt               # usual plots function 
+from pfcalibration.tools import importData,importCalib # to import binary data
+from pfcalibration.tools import savefig                # to save a figure
+from pfcalibration.tools import gaussian_param
+from pfcalibration.tools import binwidth_array
+import math
+
+
+
+# file to save the pictures
+directory = "pictures/testKNNGF/"
+
+#importation of simulated particles
+filename = 'charged_hadrons_100k.energydata'
+data1 = importData(filename)
+filename = 'prod2_200_400k.energydata'
+data2 = importData(filename)
+# we merge the 2 sets of data
+data1 = data1.mergeWith(data2)
+# we split the data in 2 sets
+data1,data2 = data1.splitInTwo()
+#data 1 -> training data
+#data 2 -> data to predict
+
+# parameters of the calibration
+lim = 150                   # if ecal + hcal > lim, ecalib = math.nan
+n_neighbors_ecal_eq_0=2000  # number of neighbors for ecal = 0
+n_neighbors_ecal_neq_0=250  # number of neighbors for ecal ≠ 0
+energystep_ecal_eq_0 = 1
+energystep_ecal_neq_0 = 5
+    
+try:
+    # We import the calibration
+    filename = "calibrations/KNNGaussianFit_162Kpart_hcal_train_ecal_eq_0_min_1.00043606758_lim_150_n_neighbors_ecal_eq_0_2000_n_neighbors_ecal_neq_0_250.calibration"
+    KNNGF = importCalib(filename)
+except FileNotFoundError:
+    # We create the calibration
+    KNNGF = data1.KNNGaussianFit(n_neighbors_ecal_eq_0=n_neighbors_ecal_eq_0,
+                                 n_neighbors_ecal_neq_0=n_neighbors_ecal_neq_0,
+                                 lim=lim,energystep_ecal_eq_0=energystep_ecal_eq_0,energystep_ecal_neq_0=energystep_ecal_neq_0,kind='cubic')
+    KNNGF.saveCalib()
+    
+classname = KNNGF.classname
+#plot 3D Training points
+fig = plt.figure(1,figsize=(5, 5))
+usplt.plot3D_training(data1)
+plt.show()
+savefig(fig,directory,classname+"_plot3D_training.png")
+savefig(fig,'img_index/',classname+"_plot3D_training.png")
+
+#plot 3D surface calibration
+fig = plt.figure(1,figsize=(5, 5))
+usplt.plot3D_surf(KNNGF,data1)
+plt.show()
+savefig(fig,directory,classname+"_plot3D_surf.png")
+savefig(fig,'img_index/',classname+"_plot3D_surf.png")
+
+#courbe de calibration pour ecal = 0
+fig = plt.figure(figsize=(10,4))
+usplt.plotCalibrationCurve(KNNGF)
+plt.show()
+savefig(fig,directory,classname+"_calibration.png")
+savefig(fig,'img_index/',classname+"_calibration.png")
+
+#ecalib/true in function of etrue
+fig = plt.figure(figsize=(10,4))
+usplt.plot_ecalib_over_etrue_functionof_etrue(KNNGF,data2)
+plt.show()
+savefig(fig,directory,classname+"_ecalib_over_etrue.png")
+savefig(fig,'img_index/',classname+"_ecalib_over_etrue.png")
+
+#histogram of ecalib and etrue
+fig = plt.figure(figsize=(10,6))
+usplt.hist_ecalib(KNNGF,data2)
+plt.show()
+savefig(fig,directory,classname+"_histograms_ecalib_etrue.png")
+savefig(fig,'img_index/',classname+"_histograms_ecalib_etrue.png")
+
+#ecalib/etrue in function of ecal,hcal
+fig = plt.figure(figsize=(10,5))
+usplt.plot_ecalib_over_etrue_functionof_ecal_hcal(KNNGF,data2)
+plt.show()
+savefig(fig,directory,classname+"_ecalib_over_etrue_functionof_ecal_hcal.png")
+savefig(fig,'img_index/',classname+"_ecalib_over_etrue_functionof_ecal_hcal.png")
+
+#ecalib/etrue gaussian fit curve
+fig = plt.figure(figsize=(10,12))
+usplt.plot_gaussianfitcurve_ecalib_over_etrue_functionof_ecal_hcal(KNNGF,data2)
+plt.show()
+savefig(fig,directory,classname+"_ecalib_over_etrue_curve.png")
+savefig(fig,'img_index/',classname+"_ecalib_over_etrue_curve.png")
